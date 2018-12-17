3d8992c2e900753c228513868c33bba94e3c57bb
suldev8 <suldev8@gamil.com>
2018-12-18 01:03:01 +0300

    make names unique

--- database_setup.py
+++ database_setup.py
@@ -19,7 +19,7 @@ class Category(Base):
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
@@ -35,7 +35,7 @@ class Item(Base):
     __tablename__ = 'item'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(80), nullable=False, primary_key=True)
+    name = Column(String(80), nullable=False, unique=True)
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
