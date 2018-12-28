f8a6f339be242a0b2d4f33d9e058f4668e1edcf6
suldev8 <suldev8@gamil.com>
2018-12-28 10:45:10 +0300

    remove localhost and port form run

--- application.py
+++ application.py
@@ -367,4 +367,5 @@ def itemJson(category_name, item_name):
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
-    app.run(host='0.0.0.0', port=5000)
+    app.debug = False
+    app.run()
